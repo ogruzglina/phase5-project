6fb3038a50c743a0d592bc4f4f66b527f9efb2fc
Olha Gruzglina <olga.zhmura@gmail.com>
2022-04-07 21:58:05 -0400

    added gem rack-cors and displayed list of chargers

--- client/App.js
+++ client/App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hi there!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Home />
   );
 }
 
---
+++ client/screens/Home.js
@@ -0,0 +1,38 @@
+import { View, Text, SafeAreaView, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import HeaderTabs from '../components/HeaderTabs'
+import axios from 'axios';
+
+export default function Home() {
+    const [ chargers, setChargers ] = useState([]);
+
+        useEffect(async () => {
+            try {
+              const res = await axios.get('http://localhost:3000/chargers');
+              const chargersList = await res.data;
+              console.log('charger', chargersList);
+              
+              setChargers(chargersList);
+            } catch (e) {
+              console.log(e);
+            }
+        }, []);
+
+    
+    return (
+        <SafeAreaView>
+            <HeaderTabs />
+
+            <View>
+                <FlatList
+                    data = { chargers }
+                    renderItem = { ({ item }) => (
+                    <View style={{marginBottom:20}}>
+                        <Text>{ item.charger_type }</Text>
+                        <Text>{ item.address }</Text>
+                    </View>)}
+                />
+            </View>
+        </SafeAreaView>
+    )
+}
