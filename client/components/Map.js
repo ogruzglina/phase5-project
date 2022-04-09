86e43b2a03f427e6e11b8269ffbd220fa043a893
Olha Gruzglina <olga.zhmura@gmail.com>
2022-04-09 19:36:29 -0400

    add stack navigation between home(map) screen and charger details

--- client/components/Map.js
+++ client/components/Map.js
@@ -3,7 +3,7 @@ import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import React, { useEffect, useState } from 'react';
 import * as Location from 'expo-location';
 
-export default function Map() {
+export default function Map({ chargers, navigation }) {
   const [location, setLocation] = useState({longitude: 0, latitude: 0});
   const [errorMsg, setErrorMsg] = useState(null);
 
@@ -31,18 +31,24 @@ export default function Map() {
         style = { styles.map }
         provider = { PROVIDER_GOOGLE }
         showsUserLocation = { true }
-        // initialRegion={{
-        //   latitude: location.latitude, 
-        //   longitude: location.longitude,
-        //   latitudeDelta: 5,
-        //   longitudeDelta: 5
-        // }}
+        followUserLocation = { true }
+        zoomEnabled = { true } 
+        initialRegion={{
+          latitude: location.latitude, 
+          longitude: location.longitude,
+          latitudeDelta: 10,
+          longitudeDelta: 10
+        }}
       >
+          {/* { chargers.map((charger, index) => (
+          <Marker key={index} title={charger.name} coordinate={charger.coordinates} />
+           ))} */}
         <Marker 
           coordinate = {{ latitude: 40.7053, longitude: -74.0139 }}
           pinColor = '#4C02DE'
         >
-          <Callout>
+          <Callout onPress = { () => navigation.navigate('ChargerDetails') }>
+              {/*  */}
             <Text>{ location.latitude }</Text>
             <Text>{ location.longitude }</Text>
           </Callout>
